b2b4e09a43e06d88969bacff99d9afb0486392f0
loganCurd <logan.curd@outlook.com>
2025-08-15 18:13:47 -0400

    add ability to save state so we don't download old episodes again

--- src/PyPod.py
+++ src/PyPod.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import re
+import os
 from datetime import datetime
 from glob import glob
 import xml.etree.ElementTree as xml
@@ -26,18 +27,18 @@ def convert_pubDate_to_datetime(pubDate):
 
 
 class Podcast:
-    def __init__(self, pod_obj):
+    def __init__(self, pod_obj, download_folder):
         self.show_title = pod_obj['show_title']
-        self.folder_name = sanitize_file_name(self.show_title)
         self.rss_feed = pod_obj['rss_feed']
         self.last_ep_date = None
         last_ep_date_string = pod_obj['last_ep_date']
-        self.file_path = ""
+        self.file_path = "{0}/{1}".format(download_folder, sanitize_file_name(self.show_title))
+        os.makedirs(self.file_path, exist_ok=True)
         if len(last_ep_date_string) > 0:
             self.last_ep_date = convert_pubDate_to_datetime(pod_obj['last_ep_date'])
 
 
-    def get_json(self):
+    def get_pod_obj(self):
         pod_dict = dict(show_title = self.show_title, rss_feed = self.rss_feed)
         pod_dict['last_ep_date'] = "" if self.last_ep_date == None else self.last_ep_date
         return pod_dict
@@ -58,14 +59,15 @@ class Podcast:
             pub_date_str = extract_text(episode.find('pubDate'))
             pub_date_datetime = convert_pubDate_to_datetime(pub_date_str)
 
-            if self.last_ep_date != None and pub_date_datetime > self.last_ep_date:
+            if self.last_ep_date != None and pub_date_datetime <= self.last_ep_date: # already downloaded
                 continue
+            
             episode_title = extract_text(episode.find('title'))
             link = extract_text(episode.find('link'))
             if link == None:
                 link = episode.find('.//enclosure').attrib['url']
 
-            file_name = "{0}.mp3".format(sanitize_file_name(episode_title))
+            file_name = "{0}/{1}.mp3".format(self.file_path, sanitize_file_name(episode_title))
 
             with session.get(link, stream=True) as download_response:
                 print("Attempting to download episode: {0}".format(episode_title))
@@ -83,20 +85,32 @@ class Podcast:
                     print("Failed to open file for download: {0}".format(file_name))
                     return
 
-            break #prevent downloading entire catalogue while testing
-
 def main():
 
     try:
-        with open('../conf/podcast.json') as f:
-            d = json.load(f)
+        with open('../conf/podcast.json', 'r') as in_file:
+            conf_data = json.load(in_file)
     except:
         print("FAILED to load config file")
+        return
 
-    podcast_list = [Podcast(y) for y in d['podcasts']]
+    download_folder = conf_data['download_directory']
+
+    podcast_list = [Podcast(y, download_folder) for y in conf_data['podcasts']]
+
+    pod_conf_list = []
 
     for pod in podcast_list:
         pod.download_new_episodes()
+        pod_conf_list.append(pod.get_pod_obj())
+
+    conf_dict = dict(download_directory = download_folder, podcasts = pod_conf_list)
+
+    try:
+        with open('../conf/podcast.json', 'w') as out_file:
+            json.dump(conf_dict, out_file)
+    except:
+        print("FAILED to write new data to config file")
  
     return 0
 
